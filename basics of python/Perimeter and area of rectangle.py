1ecc36e650b30a82577b8d5ff6682fb585efef0c
rajnishagr2210programmer <rajnishagrahari2210@gmail.com>
2026-05-20 11:39:57 +0530

    Add files via upload

---
+++ basics of python/Perimeter and area of rectangle.py
@@ -0,0 +1,6 @@
+L= int(input(' Enter length of rectangle here :'))
+B = int(input(' Enter breadth of rectangle here :'))
+Area = (L*B)
+Perimeter = (2*(L*B))
+print("Area of rectangle:", Area)
+print("Perimeter of rectangle:", Perimeter)
