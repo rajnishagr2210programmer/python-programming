ee643c775759dfd026500ed2b174cfa7fc9064d5
rajnishagr2210programmer <rajnishagrahari2210@gmail.com>
2026-07-04 12:38:21 +0530

    Add files via upload

---
+++ compairson between 2 numbers.py
@@ -0,0 +1,8 @@
+a=int(input("enter first no"))
+b=int(input("enter second no"))
+if a>b:
+    print(a, "is greater than ",b)
+elif b>a:
+    print(b,"is greater than",a)
+else:
+    print("both nos are equal to each other")
