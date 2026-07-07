876cd8f078df39948522f51f2186d3c263282013
rajnishagr2210programmer <rajnishagrahari2210@gmail.com>
2026-07-07 12:53:03 +0530

    Add files via upload

---
+++ Age and Nationality for voting.py
@@ -0,0 +1,8 @@
+while True:
+ a=eval(input("enter age"))
+ n=input("enter your nationality")
+ if a>=18 and n=='indian':
+    print("you are eligible for voting")
+ else:
+    print("you are not eligible for voting")
+ print("in",n)
