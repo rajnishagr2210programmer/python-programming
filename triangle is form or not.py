ba0f6b3e7d918b89c0c4e4d2ab37b410abcc62f6
rajnishagr2210programmer <rajnishagrahari2210@gmail.com>
2026-07-08 13:00:22 +0530

    Add files via upload

---
+++ triangle is form or not.py
@@ -0,0 +1,7 @@
+a = int(input("enter first angle"))
+b = int(input("enter second angle"))
+c = int(input("enter third  angle"))
+if  a+b+c == 180:
+    print("the angles form a triangle")
+else:
+    print("the angles do not form triangle")
