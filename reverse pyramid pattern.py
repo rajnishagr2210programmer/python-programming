df4669b3812fa9eebe6b6259bec0c75ccb8574ad
Rajnish Agrahari <rajnishagrahari2210@gmail.com>
2026-07-24 13:00:14 +0530

    Add files via upload

---
+++ reverse pyramid pattern.py
@@ -0,0 +1,8 @@
+n= int(input("enter a number"))
+for i in range (0,n):
+    for j in range (0,i):
+        print(' ',end='')
+    for j in range (0,2*(n-i)-1):
+        print("*",end='')
+    print()
+    
