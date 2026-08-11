1263999fe15d1ce963db1a194ff85f47a3de85ab
Rajnish Agrahari <rajnishagrahari2210@gmail.com>
2026-08-11 12:51:40 +0530

    Add files via upload

---
+++ gp series.py
@@ -0,0 +1,7 @@
+x=int (input("enter value of x"))
+n=int(input("entera value of n"))
+sum=0
+for i in range (0,n+1):
+      sum=sum+(x**i)
+print("sum of the following series",sum )
+ 
---
+++ other question.py
@@ -0,0 +1,7 @@
+x=int (input("enter value of x"))
+n=int(input("entera value of n"))
+sum=0
+for i in range (1,n+1):
+      sum=sum+(x**i)/i
+print("sum of the following series",sum )
+ 
